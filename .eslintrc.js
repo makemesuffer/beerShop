9ee7a87b86c5344ccea2d6bff7a8de11640bc387
makemesuffer <strigaliov4working@gmail.com>
2020-01-04 13:45:14 +0300

    Test husky, eslint and prettier

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  parser: 'babel-eslint',
-  extends: ['airbnb', 'plugin:react/recommended'],
-  env: {
-    node: true,
-    browser: true,
-  },
-};
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 
+// eslint-disable-next-line react/prefer-stateless-function
 class LandingPage extends React.Component {
   render() {
-    return (
-            <h1> Landing Page</h1>
-    );
+    return <h1> Landing Page</h1>;
   }
 }
 
