cebfbb84bda92ed3a02c72a8d66a3b0937f2acd4
makemesuffer <strigaliov4working@gmail.com>
2020-01-04 13:01:16 +0300

    Add eslint, prettier and husky

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'plugin:react/recommended'],
+  env: {
+    node: true,
+    browser: true,
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import LandingPage from "./pages/LandingPage";
+
+
+function App() {
+    return (
+        <div>
+            Tut budet routing kak ponyal
+            <LandingPage/>
+        </div>
+    );
+}
+
+export default App;
